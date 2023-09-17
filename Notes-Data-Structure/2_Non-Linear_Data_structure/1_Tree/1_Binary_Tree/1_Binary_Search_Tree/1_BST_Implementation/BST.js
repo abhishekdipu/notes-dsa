fcfbd4adc2147ca1e91c3e528f8120cf0f6ae037
Abhishek <abhishekdipu2018@gmail.com>
2023-09-17 23:56:06 +0530

    updated dsa notes

--- Notes-Data-Structure/2_Non-Linear_Data_structure/1_Tree/1_Binary_Tree/1_Binary_Search_Tree/1_BST_Implementation/BST.js
+++ Notes-Data-Structure/2_Non-Linear_Data_structure/1_Tree/1_Binary_Tree/1_Binary_Search_Tree/1_BST_Implementation/BST.js
@@ -78,9 +78,9 @@ bst.insert(7);
 bst.insert(4);
 bst.insert(9);
 bst.insert(1);
-bst.insert(6);
-bst.insert(8);
-bst.insert(10);
-bst.print();
+// bst.insert(6);
+// bst.insert(8);
+// bst.insert(10);
+// bst.print();
 
 // console.log(bst.find(9));
