522eb3fe15942311d8de39f05d2174370fc19126
Abhishek <abhishekdipu2018@gmail.com>
2024-06-16 16:33:30 +0530

    1-contains-duplicate

--- questions/2-blind-75/1-Arrays-and-Hashing/1-contains-duplicate.js
+++ questions/2-blind-75/1-Arrays-and-Hashing/1-contains-duplicate.js
@@ -1,4 +1,4 @@
-/* LC-217
+/* LC-217 (Easy)
  Given an integer array nums, return true if any value appears at least twice in the array, 
  and return false if every element is distinct.
 
@@ -14,7 +14,10 @@ Example 3:
 Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
 
-Solution : https://www.youtube.com/watch?v=3OamzN90kPg
+* Solution : https://www.youtube.com/watch?v=3OamzN90kPg
+Steps : 
+  1. make use of set.
+  2. iterate over the array, and keep adding element to set, if found in set then return true.
 */
 
 // TC : O(n), SC: O(n)
@@ -30,7 +33,10 @@ const containsDuplicate = (nums) => {
   return false;
 };
 
-// TC : O(1), SC: O(n)
+/*
+ * Approach 2 : create a new set from the given array, and compare its size with array length
+- TC : O(1), SC: O(n)
+ */
 const containsDuplicate2 = (nums) => {
   const set = new Set(nums);
   return set.size !== nums.length;
