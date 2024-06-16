124572183acdb70085242fa4c8a64d9442e32c43
Abhishek <abhishekdipu2018@gmail.com>
2024-06-16 16:57:27 +0530

    2-valid-anagram

--- questions/2-blind-75/1-Arrays-and-Hashing/2-valid-anagram.js
+++ questions/2-blind-75/1-Arrays-and-Hashing/2-valid-anagram.js
@@ -1,5 +1,4 @@
-/* LC : 242. Valid Anagram
-- Easy
+/* LC-242 :: Valid Anagram :: Easy
 
 Q. Given two strings s and t, return "true" if t is an anagram of s, and "false" otherwise.
 
@@ -16,11 +15,14 @@ Example 2:
 Input: s = "rat", t = "car"
 Output: false
 
-Sol : https://www.youtube.com/watch?v=9UtInBqnCgA
+* Solution : https://www.youtube.com/watch?v=9UtInBqnCgA
+
+ 
+
 */
 
 /* Follow this one : 
-1st sol : frequency count
+* Approach 1 : frequency count
 1. count frequency of each characters in string "s" and string "t"
 2. validate of both have same frequency for same char
 
@@ -40,27 +42,34 @@ var isAnagram = function (s, t) {
   return true;
 };
 
-//2nd solution : sort the string and compare
+/*
+* Approach 2: sort the string and compare
+* TC : O(nlogn) 
+    explanation => split : O(n); sort : O(nlogn); join : O(n)
+ */
 var isAnagram2 = function (s, t) {
   return s.split('').sort().join() === t.split('').sort().join();
 };
 
-/* Solution 3 : Using ASCII
+/* 
+* Approach 3 : Using ASCII
 - ASCII : https://www.w3schools.com/charsets/ref_html_ascii.asp
+- there are total 256 ASCII characters from 0-255
     A-Z : 65-90
     a-z : 97- 122
+* TC : O(n)
 */
-var isAnagram3 = function (s, t) {
-  let map = new Array(256).fill(0);
+const isAnagram3 = (s, t) => {
+  let arr = new Array(256).fill(0);
   for (let i = 0; i < s.length; i++) {
-    map[s.charCodeAt(i)]++;
+    arr[s.charCodeAt(i)]++;
   }
 
   for (let i = 0; i < t.length; i++) {
-    map[t.charCodeAt(i)]--;
+    arr[t.charCodeAt(i)]--;
   }
-  return map.every((value) => value === 0);
+  return arr.every((value) => value === 0);
 };
 
-console.log(isAnagram('anagram', 'nagaram')); // true
-console.log(isAnagram('rat', 'car')); //false
+console.log(isAnagram3('anagram', 'nagaram')); // true
+console.log(isAnagram3('rat', 'car')); //false
