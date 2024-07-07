f5c48bba7ee5e420b919e030e8bc58118492429d
Abhishek <abhishekdipu2018@gmail.com>
2024-07-07 15:27:45 +0530

    6-Encode-and-Decode-Strings

--- questions/2-blind-75/1-Arrays-and-Hashing/6-Encode-and-Decode-Strings.js
+++ questions/2-blind-75/1-Arrays-and-Hashing/6-Encode-and-Decode-Strings.js
@@ -20,3 +20,45 @@ Output: ["we","say",":","yes"]
 // solution : https://www.youtube.com/watch?v=B1k_sxOSgv8
 // steps:
 */
+
+class Solution {
+  /**
+   * @param {string[]} strs
+   * @returns {string}
+   */
+  encode(strs) {
+    let res = '';
+    for (let i = 0; i < strs.length; i++) {
+      const len = strs[i].length;
+      res = res + len + '#' + strs[i];
+    }
+    // console.log({encode: res}); // { encode: '4#neet4#code4#love3#you' }
+    return res;
+  }
+
+  /**
+   * @param {string} str
+   * @returns {string[]}
+   */
+  decode(str) {
+    const res = [];
+    let i = 0;
+    while (i < str.length) {
+      let temp = '';
+      if (typeof (str[i] === 'number') && str[i + 1] === '#') {
+        const sliceStart = i + 2;
+        const sliceEnd = Number(sliceStart) + Number(str[i]);
+        temp = str.slice(sliceStart, sliceEnd);
+        res.push(temp);
+        i = sliceEnd;
+        temp = '';
+      }
+    }
+    // console.log({decode: res}); // { decode: [ 'neet', 'code', 'love', 'you' ] }
+    return res;
+  }
+}
+
+const sol = new Solution();
+console.log(sol.encode(['neet', 'code', 'love', 'you'])); // 4#neet4#code4#love3#you
+console.log(sol.decode('4#neet4#code4#love3#you')); // [ 'neet', 'code', 'love', 'you' ]
