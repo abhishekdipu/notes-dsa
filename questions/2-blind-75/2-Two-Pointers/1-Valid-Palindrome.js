4a4dada95182f51e8f68ef54c82b9f0664409047
Abhishek <abhishekdipu2018@gmail.com>
2024-07-07 18:15:40 +0530

    1-Valid-Palindrome

--- questions/2-blind-75/2-Two-Pointers/1-Valid-Palindrome.js
+++ questions/2-blind-75/2-Two-Pointers/1-Valid-Palindrome.js
@@ -1,8 +1,9 @@
 /*
 https://leetcode.com/problems/valid-palindrome/description/
-
 125. Valid Palindrome
-A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric 
+characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 
 Given a string s, return true if it is a palindrome, or false otherwise.
 
@@ -27,22 +28,20 @@ Since an empty string reads the same forward and backward, it is a palindrome.
 */
 
 /**
- * @param {string} s
- * @return {boolean}
+  2 Approaches :
+    1st way :  reverse the string and check
+                  const strRev = str.split('').reverse().join('');
+                  return str === strRev;
+    2nd way : using 2 pointers
  */
-var isPalindrome = function (s) {
-  // 2 ways :
-
+const isPalindrome = (s) => {
+  // sensitize the string, remove all non-alphanumeric characters
   const str = s
-    .trim() //
+    .trim()
     .replace(/[^0-9a-zA-Z]/g, '')
     .toLowerCase();
 
-  // 1st way :  reverse the string and check
-  // const strRev = str.split('').reverse().join('');
-  // return str === strRev;
-
-  // 2nd way : using 2 pointors
+  // use 2 pointers
   let l = 0;
   let r = str.length - 1;
   while (r >= l) {
@@ -52,3 +51,19 @@ var isPalindrome = function (s) {
   }
   return true;
 };
+
+/*
+# Explanation of sensitization done as per question 
+    1. .trim() :  removes any leading and trailing whitespace from the string s.
+    2. .replace(/[^0-9a-zA-Z]/g, '') : replace all occurrences of non-alphanumeric characters to ''
+
+## Explanation For Regular Expression /[^0-9a-zA-Z]/g : 
+      - The forward slashes /.../ denote the start and end of the regular expression.
+      - ^ inside the square brackets [] is a negation operator. It means "not".
+      - 0-9 matches any digit from 0 to 9.
+      - a-z matches any lowercase letter from 'a' to 'z'.
+      - A-Z matches any uppercase letter from 'A' to 'Z'.
+    Therefore, [^0-9a-zA-Z] matches any character that is not a digit (0-9), a lowercase letter (a-z), or an uppercase letter (A-Z).
+
+  /g: The g flag stands for "global". It indicates that the regular expression should match all occurrences in the string, not just the first one.
+*/
