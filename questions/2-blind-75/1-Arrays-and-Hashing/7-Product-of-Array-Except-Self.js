820e24f24e2bc5ed749d67af6fa77ecb49f27bf0
Abhishek <abhishekdipu2018@gmail.com>
2024-07-07 15:48:20 +0530

    7-Product-of-Array-Except-Self

--- questions/2-blind-75/1-Arrays-and-Hashing/7-Product-of-Array-Except-Self.js
+++ questions/2-blind-75/1-Arrays-and-Hashing/7-Product-of-Array-Except-Self.js
@@ -17,3 +17,37 @@ Example 2:
 Input: nums = [-1,1,0,-3,3]
 Output: [0,0,9,0,0]
 */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  // const product = nums.reduce((a,c)=>a*c);
+  // console.log(product)
+  const prefix = []; // [1*1, 1*2, 2*3, 6*4] = [1,2,6,24]
+  const postfix = []; // [24*1, 12*2 ,4*3 ,1*4] = [24, 24, 12, 4]
+  const output = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const prev = i <= 0 ? 1 : prefix[i - 1];
+    prefix[i] = prev * nums[i];
+  }
+  // console.log('prefix-->', prefix);
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    const prev = i >= nums.length - 1 ? 1 : postfix[i + 1];
+    postfix[i] = prev * nums[i];
+  }
+  // console.log('postfix-->', postfix);
+
+  for (let i = 0; i < nums.length; i++) {
+    const prev = i <= 0 ? 1 : prefix[i - 1];
+    const next = i >= nums.length - 1 ? 1 : postfix[i + 1];
+    output[i] = prev * next;
+  }
+
+  return output;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4])); //[ 24, 12, 8, 6 ]
